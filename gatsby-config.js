a7de0e75e31dabb46c5db7e94491efe9153470e1
Arthur Sallé <arthursalle.pro@gmail.com>
2021-06-13 14:38:52 +0200

    setup :tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: "portfolio",
+    title: 'portfolio',
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
