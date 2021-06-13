a5b56c2f5b7e34734faaeb38416268021f0eb12e
Arthur Sallé <arthursalle.pro@gmail.com>
2021-06-13 20:02:14 +0200

    :mag: improve SEO

---
+++ src/components/organisms/Seo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import favicon from '../../images/favicon.ico';
+import ogImg from '../../images/og.png';
+import { Helmet } from 'react-helmet';
+
+const Seo = () => {
+  const faviconLink = [
+    { rel: 'icon', type: 'image/ico', sizes: '32x32', href: favicon },
+  ];
+  const title = 'Arthur Sallé | Portfolio';
+  const description =
+    'Arthur Sallé, développeur frontend, basé à Nantes et formé à la Wild Code School. Fan de la JAMstack, du CSS, des animations et attiré par le rendu visuel, j’aime coder des sites mêlant design, animations et optimisation.';
+  const url = 'https://arthur-salle.fr/';
+  return (
+    <Helmet htmlAttributes={{ lang: 'fr' }} link={faviconLink}>
+      <meta charSet='utf-8' />
+      <title>{title}</title>
+      <meta name='title' content={title} />
+      <meta name='description' content={description} />
+
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content={url} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={ogImg} />
+
+      <meta property='twitter:card' content='summary_large_image' />
+      <meta property='twitter:url' content={url} />
+      <meta property='twitter:title' content={title} />
+      <meta property='twitter:description' content={description} />
+      <meta property='twitter:image' content={ogImg} />
+    </Helmet>
+  );
+};
+
+export default Seo;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,12 @@
 import * as React from 'react';
-import { Helmet } from 'react-helmet';
 import Header from '../components/organisms/Header';
+import Seo from '../components/organisms/Seo';
 import '../styles/index.scss';
-import favicon from '../images/favicon.ico';
 
 const IndexPage = () => {
-  const faviconLink = [
-    { rel: 'icon', type: 'image/ico', sizes: '32x32', href: favicon },
-  ];
-
   return (
     <>
-      <Helmet htmlAttributes={{ lang: 'fr' }} link={faviconLink}>
-        <meta charSet='utf-8' />
-        <title>Arthur Sallé | Portfolio</title>
-      </Helmet>
+      <Seo />
       <main>
         <Header />
       </main>
